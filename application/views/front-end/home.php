f797b758f91360f66f5bda6088da46c0a9f9796e
zaka.zaidan <zaka.smansa@gmail.com>
2013-03-28 11:01:17 +0700

    Update video teaser #gajelas

--- application/views/front-end/home.php
+++ application/views/front-end/home.php
@@ -4,7 +4,7 @@
 			    <img src="http://farm5.staticflickr.com/4091/4833681624_c8b6fa82d7_z.jpg" alt="Photo by: Daniel Parks Link: http://www.flickr.com/photos/parksdh/5227623068/">
 			    <img src="http://farm5.staticflickr.com/4071/4298825842_989fa3c187_z.jpg" alt="Photo by: Mike Ranweiler Link: http://www.flickr.com/photos/27874907@N04/4833059991/">
 			    <img src="http://farm1.staticflickr.com/27/97577796_94967c3555_z.jpg" alt="Photo by: Stuart SeegerLink: http://www.flickr.com/photos/stuseeger/97577796/">
-			    <iframe width="620" height="270" src="http://www.youtube.com/embed/noHbHqrICzw?wmode=opaque" frameborder="0" allowfullscreen></iframe>
+			   	<iframe width="640" height="450" src="http://www.youtube.com/embed/zCMEm63Ypfk" frameborder="0" allowfullscreen></iframe>
 			</div>
 			<?php
 				$this->load->view('front-end/socialbox');
