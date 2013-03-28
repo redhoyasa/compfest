e798702d75dedde006e111df735704f972c65e7c
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-03-28 22:21:41 +0700

    hall of fame awal

---
+++ application/views/front-end/halloffame.php
@@ -0,0 +1,25 @@
+		<div id="content-home">
+			<div id="hof-main">
+				<ul>
+					<li>
+						<h1>Compfest 2100</h1>
+						<h2>Chanek Subardjo</h2>
+						<h3>Juara 1 Kompetisi Apps HTML7</h3>
+						<img />
+						<div id="info-kiri"></div>
+						<div id="info-kanan"></div>
+					</li>
+				</ul>
+			
+			</div>
+			<?php
+				$this->load->view('front-end/socialbox');
+			?>
+		</div>
+
+		</div>
+	</section>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/halloffame.css">
+	<script src="<?php echo base_url(); ?>assets/plugins/slider/jquery.slides.min.js" type="text/javascript"></script>
+	<script src="<?php echo base_url(); ?>assets/plugins/slider/slider.js" type="text/javascript"></script>
