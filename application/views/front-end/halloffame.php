26d9896dace16dab7622059caab7a49d847c22e6
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-03-29 00:30:32 +0700

    Hall of Fame v1
    TODO : tombol panah next dan prev

--- application/views/front-end/halloffame.php
+++ application/views/front-end/halloffame.php
@@ -1,25 +1,57 @@
 		<div id="content-home">
 			<div id="hof-main">
-				<ul>
-					<li>
-						<h1>Compfest 2100</h1>
-						<h2>Chanek Subardjo</h2>
-						<h3>Juara 1 Kompetisi Apps HTML7</h3>
-						<img />
-						<div id="info-kiri"></div>
-						<div id="info-kanan"></div>
+				<ul value="2">
+					<li id="pic-1">
+						<img class="preview" src="assets/img/halloffame/2012/lol.png" />
+						<div id="description">
+							<p>Setelah bertahun-tahun meminta orang-orang membuat program untuk membantu dia dalam
+							menyelesaikan berbagai macam masalah pribadinya yang sangat tidak penting semacam
+							main kartu dengan bebek, pak chanek akhirnya belajar programming dan berhasil menang
+							di kompetisi HTML7 di compfest 2100!!</p>
+						</div>
 					</li>
+					
+					<li id="pic-2" style="display:none">
+						<img class="preview" src="assets/img/halloffame/2012/lal.png" />
+						<div id="description">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ut nulla 
+							tristique ut dignissim risus aliquet. Sed id nulla ante. Aenean fringilla laoreet 
+							tincidunt. Nunc euismod faucibus enim, vel semper massa faucibus vitae. Suspendisse 
+							ac risus est, eget dictum justo. Sed tellus orci, auctor vitae iaculis ac, congue non 
+							sapien. Praesent sagittis eleifend nisl eu rutrum. Duis malesuada rhoncus mi eu sodales. 
+							Ut interdum rhoncus vestibulum. Fusce et erat libero.</p>
+						</div>
+					</li>
+					
 				</ul>
+				<div id="arrow-back"></div>
+				<div id="arrow-next"></div>
+				<h1>Compfest 2100</h1>
 			
 			</div>
-			<?php
-				$this->load->view('front-end/socialbox');
-			?>
+			
+			<div id="hof-list">
+				<h1>Hall of Fame</h1>
+				<ul>
+					<li>- Pak Chanek</li>
+					<li>- Bapak Chanek</li>
+					<li>- Si Chanek</li>
+					<li>- Chanek anak alay</li>
+					<li>- Bapak Chanek naik haji</li>
+					<li>- Chanek yang tertukar</li>
+					<li>- Chanek borong Hadiah</li>
+				</ul>
+			</div>
+			
+			<div id="hof-gallery">
+				<h1>Gallery</h1>
+				<img src="http://placehold.it/80x80" href="1" alt="" />
+				<img src="http://placehold.it/80x80" href="2" alt="" />
+			</div>
 		</div>
 
 		</div>
 	</section>
 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/halloffame.css">
-	<script src="<?php echo base_url(); ?>assets/plugins/slider/jquery.slides.min.js" type="text/javascript"></script>
-	<script src="<?php echo base_url(); ?>assets/plugins/slider/slider.js" type="text/javascript"></script>
+	<script src="<?php echo base_url(); ?>assets/js/halloffame.js" type="text/javascript"></script>
