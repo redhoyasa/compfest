791b52e65acec30f656a2ede38c3a4813f05e8c5
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-03-29 09:39:06 +0700

    Hall of Fame v2

--- application/views/front-end/halloffame.php
+++ application/views/front-end/halloffame.php
@@ -3,27 +3,18 @@
 				<ul value="2">
 					<li id="pic-1">
 						<img class="preview" src="assets/img/halloffame/2012/lol.png" />
-						<div id="description">
-							<p>Setelah bertahun-tahun meminta orang-orang membuat program untuk membantu dia dalam
-							menyelesaikan berbagai macam masalah pribadinya yang sangat tidak penting semacam
-							main kartu dengan bebek, pak chanek akhirnya belajar programming dan berhasil menang
-							di kompetisi HTML7 di compfest 2100!!</p>
-						</div>
 					</li>
 					
 					<li id="pic-2" style="display:none">
 						<img class="preview" src="assets/img/halloffame/2012/lal.png" />
-						<div id="description">
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ut nulla 
-							tristique ut dignissim risus aliquet. Sed id nulla ante. Aenean fringilla laoreet 
-							tincidunt. Nunc euismod faucibus enim, vel semper massa faucibus vitae. Suspendisse 
-							ac risus est, eget dictum justo. Sed tellus orci, auctor vitae iaculis ac, congue non 
-							sapien. Praesent sagittis eleifend nisl eu rutrum. Duis malesuada rhoncus mi eu sodales. 
-							Ut interdum rhoncus vestibulum. Fusce et erat libero.</p>
-						</div>
 					</li>
-					
 				</ul>
+				<div id="hof-gallery-thumbs">
+					<div id="hof-overflow-fix">
+					<img src="http://placehold.it/80x80" href="1" alt="" />
+					<img src="http://placehold.it/80x80" href="2" alt="" />
+					</div>
+				</div>
 				<div id="arrow-back"></div>
 				<div id="arrow-next"></div>
 				<h1>Compfest 2100</h1>
@@ -43,10 +34,24 @@
 				</ul>
 			</div>
 			
-			<div id="hof-gallery">
-				<h1>Gallery</h1>
-				<img src="http://placehold.it/80x80" href="1" alt="" />
-				<img src="http://placehold.it/80x80" href="2" alt="" />
+			<div id="hof-desc">
+				<h1>Description</h1>
+				
+				<div id="desc-1">
+					<p>Setelah bertahun-tahun meminta orang-orang membuat program untuk membantu dia dalam
+					menyelesaikan berbagai macam masalah pribadinya yang sangat tidak penting semacam
+					main kartu dengan bebek, pak chanek akhirnya belajar programming dan berhasil menang
+					di kompetisi HTML7 di compfest 2100!!</p>
+				</div>
+				
+				<div id="desc-2" style="display:none">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ut nulla 
+					tristique ut dignissim risus aliquet. Sed id nulla ante. Aenean fringilla laoreet 
+					tincidunt. Nunc euismod faucibus enim, vel semper massa faucibus vitae. Suspendisse 
+					ac risus est, eget dictum justo. Sed tellus orci, auctor vitae iaculis ac, congue non 
+					sapien. Praesent sagittis eleifend nisl eu rutrum. Duis malesuada rhoncus mi eu sodales. 
+					Ut interdum rhoncus vestibulum. Fusce et erat libero.</p>
+				</div>
 			</div>
 		</div>
 
--- assets/js/halloffame.js
+++ assets/js/halloffame.js
@@ -1,39 +1,72 @@
 $(document).ready(function() {
-
 	var current = 1;
 	var total = $("#hof-main > ul").attr("value");
-	
+
 	$("#arrow-next").bind("click", function(event) {
 	
 		var prevPic = "#pic-" + current;
+		var prevDesc = "#desc-" + current;
 		current++;
 		if (current > total) current = 1;
 		var currentPic = "#pic-" + current;
+		var currentDesc = "#desc-" + current;
 		
 		$(prevPic).fadeOut("slow");
+		$(prevDesc).fadeOut(0);
 		$(currentPic).fadeIn("slow");
+		$(currentDesc).fadeIn("slow");
 		
 	});
 	
 	$("#arrow-back").bind("click", function(event) {
 	
 		var prevPic = "#pic-" + current;
+		var prevDesc = "#desc-" + current;
 		current--;
 		if (current < 1) current = total;
 		var currentPic = "#pic-" + current;
+		var currentDesc = "#desc-" + current;
 		
 		$(prevPic).fadeOut("slow");
+		$(prevDesc).fadeOut(0);
 		$(currentPic).fadeIn("slow");
+		$(currentDesc).fadeIn("slow");
 		
 	});
 	
-	$("#hof-gallery > img").bind("click", function(event) {
+	$("#hof-overflow-fix > img").bind("click", function(event) {
 		var prevPic = "#pic-" + current;
+		var prevDesc = "#desc-" + current;
 		current = $(this).attr("href");
 		var currentPic = "#pic-" + current;
+		var currentDesc = "#desc-" + current;
 		
 		$(prevPic).fadeOut("slow");
+		$(prevDesc).fadeOut(0);
 		$(currentPic).fadeIn("slow");
+		$(currentDesc).fadeIn("slow");
 	});
-
+	
+	$(this).keydown(function(e){
+	
+		var prevPic = "#pic-" + current;
+		var prevDesc = "#desc-" + current;
+			
+		if (e.keyCode == 37) { 
+			current--;
+			if (current < 1) current = total;
+		} else if (e.keyCode == 39) { 
+			current++;
+			if (current > total) current = 1;
+		}
+		
+		var currentPic = "#pic-" + current;
+		var currentDesc = "#desc-" + current;
+		
+		$(prevPic).fadeOut("slow");
+		$(prevDesc).fadeOut(0);
+		$(currentPic).fadeIn("slow");
+		$(currentDesc).fadeIn("slow");
+	});
 });
+
