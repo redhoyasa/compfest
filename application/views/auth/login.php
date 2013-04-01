b2251111802d1b6eeb10c5bc625e9e5cc71dc2ed
zaka.zaidan <zaka.smansa@gmail.com>
2013-04-02 06:01:35 +0700

    rapihin login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -4,18 +4,18 @@
                 <h1>Login</h1>
             </div>
         </div>
-
+        <br>
         <div class="well" style="margin: auto; width: 40%; text-align: center;">
 			<?php if(validation_errors()) { ?>
 				<div class="alert alert-error">
 					<?php echo validation_errors(); ?>
 				</div>
 			<?php } ?>
-            <form action="<?php echo site_url('auth/login');?>" method="post">
-                <h2>Email</h2>
-                <input type="text" name="email" /><br />
-				<h2>Password</h2>
-				<input type="password" name="password" /><br />
+            <form class="inside" action="<?php echo site_url('auth/login');?>" method="post">
+                <h2>Email</h2><br>
+                <input type="text" name="email" /><br><br>
+				<h2>Password</h2><br>
+				<input type="password" name="password" /><br><br>
                 <input type="submit" class="btn btn-primary" value="login" />
             </form>
         </div>
