2489bfc04811d6776d38b2115dd41be0ef945731
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-04-01 21:12:49 +0700

    Overview - Hard Sketch
    Halaman awal Overview (Hall of Fame bagian dari overview)

---
+++ application/views/front-end/overview.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/overview.css" type="text/css" media="screen" />
+
+<div id="overview-wrapper">
+	<div id="overview-header">
+		<div class="choose">
+			<div id="overview-2009"></div>
+			<p>2009</p>
+		</div>
+		<div class="choose">
+			<div id="overview-2010"></div>
+			<p>2010</p>
+		</div>
+		<div class="choose">
+			<div id="overview-2011"></div>
+			<p>2011</p>
+		</div>
+		<div class="choose">
+			<div id="overview-2012"></div>
+			<p>2012</p>
+		</div>
+	</div>
+	
+	<div id="overview-content">
+		<h1>Compfest Beyond Times</h1>
+		<h2>Command the Past, Conquer The Future</h1>
+		<p>Pilih salah satu tahun untuk melihat <i>overview</i> acara Compfest tahun tersebut!</p>
+	</div>
+</div>
