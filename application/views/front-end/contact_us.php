510ae3bbe1651073be82a61ace34cb4240135406
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-04-04 06:39:04 +0700

    Contact/Team page
    Udah jadi, tinggal nunggu gambar sama spesifik font dari badur :3

--- application/views/front-end/contact_us.php
+++ application/views/front-end/contact_us.php
@@ -4,8 +4,8 @@
 </div>
 
 <div id="contact-wrapper">
-	<h1>The Team</h1>
 	<img src="<?php echo base_url(); ?>assets/img/contact/pic1.png" alt="Harusnya muncul gambar disini" style="float:left" />
+	<h1>The Team</h1>
 	<p>Gua ga tau mau diisi apaan ini, jadi gua kasih lorem ipsum aja yak : Lorem ipsum dolor 
 	sit amet, consectetur adipiscing elit. Duis porta lacus ut eros convallis a tempor neque laoreet. Vivamus posuere, 
 	leo vitae placerat porttitor, est magna imperdiet diam, eu ornare metus justo eget dolor. Nulla quis est a massa 
@@ -16,7 +16,19 @@
 	Morbi scelerisque enim vel lacus tempor luctus. Nunc ultricies risus quis libero suscipit laoreet nec ultrices 
 	lacus. </p>
 	
+	<br />
+	
 	<img src="<?php echo base_url(); ?>assets/img/contact/pic2.png" alt="Harusnya muncul gambar disini" style="float:right" />
+	<p>Gua ga tau mau diisi apaan ini, jadi gua kasih lorem ipsum aja yak : Lorem ipsum dolor 
+	sit amet, consectetur adipiscing elit. Duis porta lacus ut eros convallis a tempor neque laoreet. Vivamus posuere, 
+	leo vitae placerat porttitor, est magna imperdiet diam, eu ornare metus justo eget dolor. Nulla quis est a massa 
+	consequat luctus in eu risus. In vel erat et augue consectetur ultrices vel quis leo. Proin mauris purus, 
+	volutpat nec aliquet sit amet, adipiscing id libero. Vestibulum sit amet mollis dolor. Class aptent taciti 
+	sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi suscipit hendrerit sapien eget 
+	tristique. Phasellus tincidunt purus eget magna vulputate vehicula. Suspendisse venenatis vehicula luctus. 
+	Morbi scelerisque enim vel lacus tempor luctus. Nunc ultricies risus quis libero suscipit laoreet nec ultrices 
+	lacus. </p>
+	
 	<p>Gua ga tau mau diisi apaan ini, jadi gua kasih lorem ipsum aja yak : Lorem ipsum dolor 
 	sit amet, consectetur adipiscing elit. Duis porta lacus ut eros convallis a tempor neque laoreet. Vivamus posuere, 
 	leo vitae placerat porttitor, est magna imperdiet diam, eu ornare metus justo eget dolor. Nulla quis est a massa 
@@ -34,10 +46,11 @@
 		<li>
 			<a href="mailto:mail1@compfest.web.id">
 				<h2>Kontak 1</h2>
+				<img src="<?php echo base_url(); ?>assets/img/contact/contact1.gif" alt="Harusnya muncul gambar disini" />
 				<table>
-					<tr><td>Phone</td><td>:</td><td>0888888xxx</td></tr>
-					<tr><td>E-Mail</td><td>:</td><td>mail1@compfest.web.id</td></tr>
-					<tr><td>Friendster</td><td>:</td><td>AqYu ChAy4nK k4Mu</td></tr>
+					<tr><td>Phone</td><td>: </td><td>0888888xxx</td></tr>
+					<tr><td>E-Mail</td><td>: </td><td>mail1@compfest.web.id</td></tr>
+					<tr><td>Friendster</td><td>: </td><td>AqYu ChAy4nK k4Mu</td></tr>
 				</table>
 			</a>
 		</li>
@@ -45,10 +58,11 @@
 		<li>
 			<a href="mailto:mail2@compfest.web.id">
 				<h2>Kontak 2</h2>
+				<img src="<?php echo base_url(); ?>assets/img/contact/contact2.gif" alt="Harusnya muncul gambar disini" />
 				<table>
-					<tr><td>Phone</td><td>:</td><td>0888888xxx</td></tr>
-					<tr><td>E-Mail</td><td>:</td><td>mail1@compfest.web.id</td></tr>
-					<tr><td>Friendster</td><td>:</td><td>AqYu ChAy4nK k4Mu</td></tr>
+					<tr><td>Phone</td><td>: </td><td>0888888xxx</td></tr>
+					<tr><td>E-Mail</td><td>: </td><td>mail1@compfest.web.id</td></tr>
+					<tr><td>Friendster</td><td>: </td><td>AqYu ChAy4nK k4Mu</td></tr>
 				</table>
 			</a>
 		</li>
@@ -56,10 +70,11 @@
 		<li>
 			<a href="mailto:mail3@compfest.web.id">
 				<h2>Kontak 3</h2>
+				<img src="<?php echo base_url(); ?>assets/img/contact/contact3.gif" alt="Harusnya muncul gambar disini" />
 				<table>
-					<tr><td>Phone</td><td>:</td><td>0888888xxx</td></tr>
-					<tr><td>E-Mail</td><td>:</td><td>mail1@compfest.web.id</td></tr>
-					<tr><td>Friendster</td><td>:</td><td>AqYu ChAy4nK k4Mu</td></tr>
+					<tr><td>Phone</td><td>: </td><td>0888888xxx</td></tr>
+					<tr><td>E-Mail</td><td>: </td><td>mail1@compfest.web.id</td></tr>
+					<tr><td>Friendster</td><td>: </td><td>AqYu ChAy4nK k4Mu</td></tr>
 				</table>
 			</a>
 		</li>
