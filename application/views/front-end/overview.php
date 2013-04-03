4447d0f155927490c78a24443bd04835cb17493a
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-04-03 10:51:43 +0700

    Overview Page Quickfix

--- application/views/front-end/overview.php
+++ application/views/front-end/overview.php
@@ -2,22 +2,22 @@
 
 <div id="overview-wrapper">
 	<div id="overview-header">
-		<div class="choose">
+		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
 			<div id="overview-2009"></div>
 			<p>2009</p>
-		</div>
-		<div class="choose">
+		</a>
+		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
 			<div id="overview-2010"></div>
 			<p>2010</p>
-		</div>
-		<div class="choose">
+		</a>
+		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
 			<div id="overview-2011"></div>
 			<p>2011</p>
-		</div>
-		<div class="choose">
+		</a>
+		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
 			<div id="overview-2012"></div>
 			<p>2012</p>
-		</div>
+		</a>
 	</div>
 	
 	<div id="overview-content">
