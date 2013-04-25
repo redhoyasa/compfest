265a0d424d4434b0bf45ce0cff03c89f61f6694f
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-04-25 09:47:12 +0700

    Overview-page completed (compfest/halloffame)

--- application/views/front-end/halloffame.php
+++ application/views/front-end/halloffame.php
@@ -1,57 +1,70 @@
-		<div id="content-home">
-			<div id="hof-main">
-				<ul value="2">
-					<li id="pic-1">
-						<img class="preview" src="assets/img/halloffame/2012/lol.png" />
-						<div id="description">
-							<p>Setelah bertahun-tahun meminta orang-orang membuat program untuk membantu dia dalam
-							menyelesaikan berbagai macam masalah pribadinya yang sangat tidak penting semacam
-							main kartu dengan bebek, pak chanek akhirnya belajar programming dan berhasil menang
-							di kompetisi HTML7 di compfest 2100!!</p>
-						</div>
-					</li>
-					
-					<li id="pic-2" style="display:none">
-						<img class="preview" src="assets/img/halloffame/2012/lal.png" />
-						<div id="description">
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ut nulla 
-							tristique ut dignissim risus aliquet. Sed id nulla ante. Aenean fringilla laoreet 
-							tincidunt. Nunc euismod faucibus enim, vel semper massa faucibus vitae. Suspendisse 
-							ac risus est, eget dictum justo. Sed tellus orci, auctor vitae iaculis ac, congue non 
-							sapien. Praesent sagittis eleifend nisl eu rutrum. Duis malesuada rhoncus mi eu sodales. 
-							Ut interdum rhoncus vestibulum. Fusce et erat libero.</p>
-						</div>
-					</li>
-					
-				</ul>
-				<div id="arrow-back"></div>
-				<div id="arrow-next"></div>
-				<h1>Compfest 2100</h1>
-			
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/overview-page.css">
+<script src="<?php echo base_url(); ?>assets/js/halloffame.js" type="text/javascript"></script>
+
+<div id="overview-page-wrapper" class="clearfix">
+	<div id="overview-page-gallery">
+		<div id="msg_slideshow" class="msg_slideshow">
+			<div id="msg_wrapper" class="msg_wrapper">
+				</div>
+			<div id="msg_controls" class="msg_controls"><!-- right has to animate to 15px, default -110px -->
+				<a href="#" id="msg_grid" class="msg_grid"></a>
+				<a href="#" id="msg_prev" class="msg_prev"></a>
+				<a href="#" id="msg_pause_play" class="msg_pause"></a><!-- has to change to msg_play if paused-->
+				<a href="#" id="msg_next" class="msg_next"></a>
 			</div>
-			
-			<div id="hof-list">
-				<h1>Hall of Fame</h1>
-				<ul>
-					<li>- Pak Chanek</li>
-					<li>- Bapak Chanek</li>
-					<li>- Si Chanek</li>
-					<li>- Chanek anak alay</li>
-					<li>- Bapak Chanek naik haji</li>
-					<li>- Chanek yang tertukar</li>
-					<li>- Chanek borong Hadiah</li>
-				</ul>
-			</div>
-			
-			<div id="hof-gallery">
-				<h1>Gallery</h1>
-				<img src="http://placehold.it/80x80" href="1" alt="" />
-				<img src="http://placehold.it/80x80" href="2" alt="" />
+			<div id="msg_thumbs" class="msg_thumbs"><!-- top has to animate to 0px, default -230px -->
+				<div class="msg_thumb_wrapper">
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/1.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/1.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/2.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/2.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/3.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/3.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/4.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/4.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/5.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/5.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/6.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/6.jpg"/></a>
+				</div>
+				<div class="msg_thumb_wrapper" style="display:none;">
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/7.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/7.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/8.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/8.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/9.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/9.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/10.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/10.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/11.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/11.jpg"/></a>
+					<a href="#"><img src="<?php echo base_url(); ?>assets/img/overview/2009/thumbs/12.jpg" alt="<?php echo base_url(); ?>assets/img/overview/2009/12.jpg"/></a>
+				</div>
+				<a href="#" id="msg_thumb_next" class="msg_thumb_next"></a>
+				<a href="#" id="msg_thumb_prev" class="msg_thumb_prev"></a>
+				<a href="#" id="msg_thumb_close" class="msg_thumb_close"></a>
+				<span class="msg_loading"></span><!-- show when next thumb wrapper loading -->
 			</div>
 		</div>
-
+	</div>
+	
+	<div id="overview-page-content">
+		
+		<div id="overview-page-about">
+			<img src="<?php echo base_url(); ?>assets/img/overview/2009/logo.png" />
 		</div>
-	</section>
-
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/halloffame.css">
-	<script src="<?php echo base_url(); ?>assets/js/halloffame.js" type="text/javascript"></script>
+		
+		<div id="overview-page-quickreview">
+			<h1>Compfest 2100</h1>
+			<p>Compfest 2100 merupakan acara bergengsi yang diadakan setiap tahunnya oleh
+			Bapak Chanek, sebagai ajang untuk membantu pak chanek dalam menyelesaikan berbagai
+			masalah bersama bebek-bebeknya.</p>
+			
+			<p>Di tahun 2100 ini, ada inovasi-inovasi kompetisi yang belum pernah ada sebelumnya
+			di acara compfest. Beberapa diantaranya adalah kompetisi <strong>santet eyang subur</strong> dengan 
+			bantuan bahasa pemrograman Java SE (Santet Edition), kompetisi <strong>Pengeditan video
+			DEMI TU..HAAAN..</strong>, dan kompetisi <strong>DOTA dengan menggunakan touchpad dan mode -arshscom.</strong></p>
+		</div>
+		
+		<div id="overview-page-halloffame">
+			<h1>Hall of Fame</h1>
+			<ul>
+				<li>Pak Chanek | <strong>Juara 1 kompetisi DOTA -arshscom</strong></li>
+				<li>Pak Chanek | <strong>Juara 1 kompetisi DOTA -arshscom</strong></li>
+				<li>Pak Chanek | <strong>Juara 1 kompetisi DOTA -arshscom</strong></li>
+				<li>Pak Chanek | <strong>Juara 1 kompetisi DOTA -arshscom</strong></li>
+				<li>Pak Chanek | <strong>Juara 1 kompetisi DOTA -arshscom</strong></li>
+			</ul>
+		</div>
+		
+	</div>
+</div>
--- application/views/front-end/overview.php
+++ application/views/front-end/overview.php
@@ -1,22 +1,64 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/overview.css" type="text/css" media="screen" />
 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/overview.js"></script>
+
 <div id="overview-wrapper">
 	<div id="overview-header">
-		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
-			<div id="overview-2009"></div>
-			<p>2009</p>
+		<a class="choose" id="ch1" href="<?php echo base_url(); ?>overview/2009">
+			<div id="ch1content" style="display:none">
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2009color.png" />
+				<br />
+				<br />
+				2009
+			</div>
+			<span>
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2009black.png" />
+				<br />
+				<br />
+				2009
+			</span>
 		</a>
-		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
-			<div id="overview-2010"></div>
-			<p>2010</p>
+		<a class="choose" id="ch2" href="<?php echo base_url(); ?>overview/2010">
+			<div id="ch2content" style="display:none">
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2010color.png" />
+				<br />
+				<br />
+				2010
+			</div>
+			<span>
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2010black.png" />
+				<br />
+				<br />
+				2010
+			</span>
 		</a>
-		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
-			<div id="overview-2011"></div>
-			<p>2011</p>
+		<a class="choose" id="ch3" href="<?php echo base_url(); ?>overview/2011">
+			<div id="ch3content" style="display:none">
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2011color.png" />
+				<br />
+				<br />
+				2011
+			</div>
+			<span>
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2011black.png" />
+				<br />
+				<br />
+				2011
+			</span>
 		</a>
-		<a class="choose" href="<?php echo base_url(); ?>overview/2009">
-			<div id="overview-2012"></div>
-			<p>2012</p>
+		<a class="choose" id="ch4" href="<?php echo base_url(); ?>overview/2012">
+			<div id="ch4content" style="display:none">
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2012color.png" />
+				<br />
+				<br />
+				2012
+			</div>
+			<span>
+				<img src="<?php echo base_url(); ?>assets/img/overview/logo2012black.png" />
+				<br />
+				<br />
+				2012
+			</span>
 		</a>
 	</div>
 	
