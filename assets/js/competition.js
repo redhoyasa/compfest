ef7a28d1a9b88496824f1f7f16db31bf6a6d6d9b
pandhu <pandhu.hutomo@ui.ac.id>
2013-04-28 17:39:39 +0700

    update. competition

--- assets/js/competition.js
+++ assets/js/competition.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
 	$(".fancybox").click(function(){
 		var $nowShow = $(".competition-wrapper:visible");
+		var $selected = $(".fancybox");
 		var tinggi;
 		$nowShow.hide();
+		$selected.children().fadeTo("fast", 1.0);
 		if ($(this).attr('id') == 'cp-link') {
+			$(this).children().fadeTo("fast", 0.2);
 			$nowShow = $('#cp');
 			height = 30 + $nowShow.height();
-			$(".content-wrapper").height(tinggi);
+			$(".content-wrapper").height(height); 
 			$nowShow.fadeIn();
 		}
 		if ($(this).attr('id') == 'open-link') {
+			$(this).children().fadeTo("fast", 0.2);
 			$nowShow = $('#open');
 			height = 30 + $nowShow.height();
 			$(".content-wrapper").height(height);
 			$nowShow.fadeIn();
 		}
 		if ($(this).attr('id') == 'robot-link') {
+			$(this).children().fadeTo("fast", 0.2);
 			$nowShow = $('#robot');
 			height = 30 + $nowShow.height();
 			$(".content-wrapper").height(height);
 			$nowShow.fadeIn();
 		}
 		if ($(this).attr('id') == 'wa-link') {
+			$(this).children().fadeTo("fast", 0.2);
 			$nowShow = $('#wa');
 			height = 30 + $nowShow.height();
 			$(".content-wrapper").height(height);
