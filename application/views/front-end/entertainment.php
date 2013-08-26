a5525c498dc38bf1793daf5120449cbff4e618d1
pandhu <pandhu.hutomo@ui.ac.id>
2013-08-26 21:21:25 +0700

    wowslider

--- application/views/front-end/entertainment.php
+++ application/views/front-end/entertainment.php
@@ -1,43 +1,30 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/slider/carousel.css" type="text/css" media="screen" />
-
 <link rel="stylesheet" type="text/css" href="<?php site_url();?>assets/css/entertainment.css">
 
-<script src="<?php echo base_url(); ?>assets/plugins/slider/carousel.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/slider/holder.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/slider/transitions.min.js" type="text/javascript"></script>
+<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/plugins/WOWSlider/engine1/style.css" />
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/WOWSlider/engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+</head>
+<body style="background-color:#d7d7d7">
+	<!-- Start WOWSlider.com BODY section -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+<li><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/images/headerbg.png" alt="header-bg" title="MAIN EVENT COMPFEST" id="wows1_0"/>Balairung Universitas Indonesia, 21-22 September 2013</li>
+<li><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/images/headerbg.png" alt="header-bg" title="MAIN EVENT COMPFEST" id="wows1_0"/></li>
+<li><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/images/headerbg0.gif" alt="header-bg" title="header-bg" id="wows1_1"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="header-bg"><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/tooltips/headerbg.jpg" alt="header-bg"/>1</a>
+<a href="#" title="header-bg"><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/tooltips/headerbg.jpg" alt="header-bg"/>1</a>
+<a href="#" title="header-bg"><img src="<?php echo base_url(); ?>assets/plugins/WOWSlider/data1/tooltips/headerbg_0.jpg" alt="header-bg"/>2</a>
+</div></div>
 
-<div id="this-carousel-id" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="<?php echo base_url(); ?>assets/img/header-red.png" width="1200px" height="480px" alt="" />
-      <div class="carousel-caption">
-        <p>Event merah bro</p>
-      </div>
-    </div>
-    <div class="item">
-      <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/competition/header-bg.png" alt="" />
-      <div class="carousel-caption">
-        <p>Caption text here</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/1200x480" alt="" />
-      <div class="carousel-caption">
-        <p>Caption text here</p>
-      </div>
-    </div>
-    <div class="item">
-      <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/about/header-web-green.png" alt="" />
-      <div class="carousel-caption">
-        <p>Caption text here</p>
-      </div>
-    </div>
-  </div><!-- /.carousel-inner -->
-  <!--  Next and Previous controls below
-        href values must reference the id for this carousel -->
-    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev"><span class="icon-prev"></span></a>
-    <a class="carousel-control right" href="#this-carousel-id" data-slide="next"><span class="icon-next"></span></a>
-</div>
+	<div class="ws_shadow"></div>
+	</div>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/WOWSlider/engine1/wowslider.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/WOWSlider/engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+
 <div id="entertain-wrapper">	
 	<h2 class="title-content">Entertainment</h2>
 	<div id="guest-star" class="entertain-content"> 	
