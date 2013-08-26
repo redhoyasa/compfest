c198c1d4b813a29ed045bcb699bdb1dbbe1b675f
zaka.zaidan <zaka.smansa@gmail.com>
2013-08-26 17:17:16 +0700

    update slider for home

--- application/views/front-end/home.php
+++ application/views/front-end/home.php
@@ -1,31 +1,44 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/slider/carousel.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/home.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" type="text/css" media="screen" />
 
-<script src="<?php echo base_url(); ?>assets/plugins/slider/carousel.min.js" type="text/javascript"></script>
-<!--script type="text/javascript" src="<?php echo base_url(); ?>assets/js/home.js"></script-->
-<script src="<?php echo base_url(); ?>assets/plugins/slider/transitions.min.js" type="text/javascript"></script>
 
 <div id="middle">
-		<!-- CAROUSEL (SLIDER) AREA -->
-<div id="slider" class="carousel slide">
-<!--ol class="carousel-indicators">
-<li data-target=".carousel" data-slide-to="0" class="active"></li>
-<li data-target=".carousel" data-slide-to="1"></li>
-<li data-target=".carousel" data-slide-to="2"></li>
-</ol-->
 
-<!-- Carousel items -->
-<div class="carousel-inner">
-<div class="active item">
-<img width="1180" src="<?php echo base_url(); ?>assets/img/header-red.png">
-
+<div id="this-carousel-id" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="<?php echo base_url(); ?>assets/img/header-red.png" width="1200px" height="480px" alt="" />
+      <div class="carousel-caption">
+        <p>Event merah bro</p>
+      </div>
+    </div>
+    <div class="item">
+      <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/competition/header-bg.png" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1200x480" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+    <div class="item">
+      <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/about/header-web-green.png" alt="" />
+      <div class="carousel-caption">
+        <p>Caption text here</p>
+      </div>
+    </div>
+  </div><!-- /.carousel-inner -->
+  <!--  Next and Previous controls below
+        href values must reference the id for this carousel -->
+    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev"><span class="icon-prev"></span></a>
+    <a class="carousel-control right" href="#this-carousel-id" data-slide="next"><span class="icon-next"></span></a>
 </div>
 
-<!-- Carousel nav>
-<a class="carousel-control left" href=".carousel" data-slide="prev">&lsaquo;</a>
-<a class="carousel-control right" href=".carousel" data-slide="next">&rsaquo;</a-->
-</div>
-
+<br><br>
 <div id="box">
 <iframe width="530" height="325" src="http://www.youtube.com/embed/auHYjYb6ogw" frameborder="0" allowfullscreen></iframe>
 </div>
@@ -109,3 +122,9 @@
 	</div>
 </div>  
 </div>
+
+
+<script src="<?php echo base_url(); ?>assets/plugins/slider/carousel.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/slider/holder.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/home.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/slider/transitions.min.js" type="text/javascript"></script>
