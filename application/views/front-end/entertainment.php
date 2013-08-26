7610ce56092ecfcc3eac476512fd276a6ebc1f0e
pandhu <pandhu.hutomo@ui.ac.id>
2013-08-26 17:20:39 +0700

    entertainment

---
+++ application/views/front-end/entertainment.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/slider/nivo-slider.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/slider/default/default.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/home.css" type="text/css" media="screen" />
+<script src="<?php echo base_url(); ?>assets/plugins/slider/jquery.nivo.slider.pack.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="<?php site_url();?>assets/css/entertainment.css">
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/home.js"></script>
+
+
+	<div id="slider-wrapper" class="theme-default">
+	    <div id="slider" class="nivoSlider">
+	        <img class="imgSlider" src="<?php echo base_url(); ?>assets/img/placehold/1180x480.gif" alt="" title="COMPFEST MAIN EVENT"/>
+	        <img class="imgSlider" src="<?php echo base_url(); ?>assets/img/placehold/1180x480.gif" alt="" title="COMPFEST MAIN EVENT 2"/>
+	    </div>
+	</div>
+	<div id="htmlcaption" class="nivo-html-caption">
+	    <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
+	</div>
+<div id="entertain-wrapper">	
+	<h2 class="title-content">Entertainment</h2>
+	<div id="guest-star" class="entertain-content"> 	
+		<h3 class="title-content">Guest Star</h3>
+		<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/600x200.gif" />
+	</div>
+	<div id="band" class="entertain-content">
+		<h3 class="title-content">Bands</h3>
+		<table>
+			<tr>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div id="perform" class="entertain-content">
+		<h3 class="title-content">Performances</h3>
+		<table>
+			<tr>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+				<td>
+					<img class="team-img" src="<?php echo base_url(); ?>assets/img/placehold/300x200.gif" />	
+				</td>
+			</tr>
+		</table>
+	</div>
+</div>
