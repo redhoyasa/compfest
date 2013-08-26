32990ae3069175a4961d45b666c83194472e5e5b
Thirafi Dide (Yusei) <jurnaldide@gmail.com>
2013-08-26 23:36:10 +0700

    Playground v0.8.1

--- application/views/front-end/playground.php
+++ application/views/front-end/playground.php
@@ -1,11 +1,344 @@
-		<div id="container">
-			<h2 class="content-title">Playground</h2>
-							
-				<div class="seminar-instance">
+		<div id="this-carousel-id" class="carousel slide">
+		 
+		 <div class="carousel-inner">
+			<div class="item active">
+			  <img src="<?php echo base_url(); ?>assets/img/header-red.png" width="1200px" height="480px" alt="" />
+			  <div class="carousel-caption">
+				<p>Event merah bro</p>
+			  </div>
+			</div>
+			<div class="item">
+			  <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/competition/header-bg.png" alt="" />
+			  <div class="carousel-caption">
+				<p>Caption text here</p>
+			  </div>
+			</div>
+			<div class="item">
+			  <img src="http://placehold.it/1200x480" alt="" />
+			  <div class="carousel-caption">
+				<p>Caption text here</p>
+			  </div>
+			</div>
+			<div class="item">
+			  <img width="1200" height="480" src="<?php echo base_url(); ?>assets/img/about/header-web-green.png" alt="" />
+			  <div class="carousel-caption">
+				<p>Caption text here</p>
+			  </div>
+			</div>
+		  </div><!-- /.carousel-inner -->
+		  <!--  Next and Previous controls below
+				href values must reference the id for this carousel -->
+			<a class="carousel-control left" href="#this-carousel-id" data-slide="prev"><span class="icon-prev"></span></a>
+			<a class="carousel-control right" href="#this-carousel-id" data-slide="next"><span class="icon-next"></span></a>
+		</div>
 
-					COMING SOON!
+		<div id="container">
+			<h1 class="content-title">Playground</h1>
+							
+				<div class="clearfix seminar-instance">
+					<div class="playground-main-slider-button">
+						<a data-dir="prev" href="#"><img src="<?php echo base_url(); ?>assets/img/playground/slider-left.png"/></a>
+					</div>
+					
+					<div class="playground-main-slider">
+						<ul>
+							<li><a class="playground-box-slider" href="1" data-axis="50" data-ordinate="80">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Tinker Games </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="2" data-axis="100" data-ordinate="200">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Badr Interactive </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="3" data-axis="700" data-ordinate="150">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Ar&Co </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							
+							<li><a class="playground-box-slider" href="4" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Tinker Games </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="5" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Badr Interactive </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="6" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Ar&Co </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="7" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Tinker Games </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="8" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Badr Interactive </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+							<li><a class="playground-box-slider" href="9" data-axis="50" data-ordinate="100">
+								<img src="<?php echo base_url(); ?>assets/img/playground/slider-img-1.png">
+								<p class="playground-box-caption"> Ar&Co </p>
+								<p class="playground-box-caption2"> Game </p>
+							</a></li>
+						</ul>
+					</div>
+					
+					<div class="playground-main-slider-button">
+						<a data-dir="next" href="#"><img src="<?php echo base_url(); ?>assets/img/playground/slider-right.png"/></a>
+					</div>
+					
+					<div style="clear:both;"></div>
+					
+					<div class="playground-main-desc">
+						<div class="playground-desc-mover">
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+					
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+					
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="playground-main-desc-floater">
+							<div class="playground-main-desc-text">
+								<img src="<?php echo base_url(); ?>assets/img/playground/desc-img-1.png"/>
+								<h1>Chanek!</h1>
+								<h2>Game Developer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+								mollit anim id est laborum.</p>
+							</div>
+							<div class="playground-side-desc-text">
+								<h1>Challenges!</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+								<h1>Prize*</h1>
+								<ul>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+									<li>Lorem ipsum lala</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					</div>
+					
+					<div style="clear:both;"></div>
+					
+					<div class="playground-map">
+						<div class="playground-map-pointer"></div>
+					</div>
 				</div>
 				
-	</div>	
+				
+	</div>
 
 		<link rel="stylesheet" type="text/css" href="<?php site_url();?>assets/css/playground.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/slider/carousel.css" type="text/css" media="screen" />
+		
+		<script src="<?php echo base_url(); ?>assets/plugins/slider/carousel.min.js" type="text/javascript"></script>
+		<script src="<?php echo base_url(); ?>assets/plugins/slider/holder.js" type="text/javascript"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/playground.js"></script>
+		<script src="<?php echo base_url(); ?>assets/plugins/slider/transitions.min.js" type="text/javascript"></script>
